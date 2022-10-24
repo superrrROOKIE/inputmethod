a18c0e9bc8cd2c2b5ce40039be06f66944df1bc6
haoyi <haoyi@eduroam-int-dhcp-97-94-185.ucl.ac.uk>
2022-10-24 12:06:06 +0100

    Initial commit

---
+++ app/src/main/java/com/example/inputmethodv1/InputMethodSetting.java
@@ -0,0 +1,15 @@
+package com.example.inputmethodv1;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class InputMethodSetting extends Activity
+{
+	@Override
+	protected void onCreate(Bundle savedInstanceState)
+	{		
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.setting);
+	}
+
+}
